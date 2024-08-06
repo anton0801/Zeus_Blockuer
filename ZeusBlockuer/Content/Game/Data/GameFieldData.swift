2b024e2e4ea4871795c76bbcb75ce3cb272fa3d1
Anton <danilovanton724@gmail.com>
2024-08-06 16:15:08 +0200

    Second Commit

--- ZeusBlockuer/Content/Game/Data/GameFieldData.swift
+++ ZeusBlockuer/Content/Game/Data/GameFieldData.swift
@@ -22,6 +22,18 @@ let gameFieldDataItems = [
         [.blue, .orange, .blue], [.orange, .blue, .orange]
     ]),
     GameFieldData(countBlocks: 6, blockOrientation: .horizontal, colorFirst: .orange, initialPositions: [
-        [.blue, .orange, .blue], [.orange, .blue, .orange]
+        [.blue, .orange, .blue], [.blue, .blue, .orange]
+    ]),
+    GameFieldData(countBlocks: 6, blockOrientation: .vertical, colorFirst: .blue, initialPositions: [
+        [.blue, .orange], [.blue, .orange], [.orange, .blue]
+    ]),
+    GameFieldData(countBlocks: 4, blockOrientation: .vertical, colorFirst: .orange, initialPositions: [
+        [.orange, .blue], [.orange, .blue]
+    ]),
+    GameFieldData(countBlocks: 6, blockOrientation: .horizontal, colorFirst: .blue, initialPositions: [
+        [.orange, .orange, .blue], [.blue, .orange, .blue]
+    ]),
+    GameFieldData(countBlocks: 4, blockOrientation: .horizontal, colorFirst: .blue, initialPositions: [
+        [.orange, .orange], [.blue, .orange]
     ]),
 ]
